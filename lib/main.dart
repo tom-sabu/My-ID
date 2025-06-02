f50192173bdbd4b40c43d679aa97f878681d2e4d
Tom <tomsabu@tutamail.com>
2025-06-02 18:20:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Info Card',
+      home: MyInfoPage(),
+      debugShowCheckedModeBanner: false,
+    )
+  }
+}
+
+class MyInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Digital IC'),
+        backgroundColor: Colors.blueGrey[700],
+      ),
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(
+        child: Text( 'Info Car'),
+      ),
+    )
+  }
+}
